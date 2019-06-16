a290aea10043782537ebb727b3e12979384e897c
Alfred <alfred.adom@gmail.com>
2019-06-16 00:06:28 -0400

    added comments

--- js/script.js
+++ js/script.js
@@ -1,50 +1,60 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+// Global variables
+const lists = document.querySelectorAll(".student-item");
+const numberPerPage = 10;
 
+// Function to dislay 10 students per page
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+const showPage = (list, page) => {
+  let pageStart = page * numberPerPage - numberPerPage;
+  let pageEnd = page * numberPerPage;
 
+  for (let i = 0; i < list.length; i++) {
+    if (i >= pageStart && i < pageEnd) {
+      list[i].style.display = "block";
+    } else {
+      list[i].style.display = "none";
+    }
+  }
+};
 
+// Function to create tags to be appended
 
+const appendPageLinks = list => {
+  let numberOfPages = Math.ceil(list.length / numberPerPage);
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+  const div = document.createElement("div");
+  div.className = "pagination";
+  document.querySelector(".page").appendChild(div);
+  const ul = document.createElement("ul");
+  div.appendChild(ul);
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+  for (let i = 0; i < numberOfPages; i++) {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    a.textContent = `${i + 1}`;
 
+    if (i === 0) {
+      a.className = "active";
+    }
+    ul.appendChild(li);
+    li.appendChild(a);
+  }
 
+  // Click listener
 
+  ul.addEventListener("click", e => {
+    const link = e.target;
+    if (link.tagName === "A") {
+      pageNumber = e.target.textContent;
+      const clicked = document.querySelectorAll(".pagination a");
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+      for (let i = 0; i < clicked.length; i++) {
+        clicked[i].classList.remove("active");
+      }
+      link.className = "active";
+    }
+    showPage(list, pageNumber);
+  });
+};
 
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+appendPageLinks(lists);
